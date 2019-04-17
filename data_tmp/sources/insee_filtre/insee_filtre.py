77838658c5e99bf5dc4496d8a7656d9f3007cd39
jimenarl <jimena@melix.net>
2019-04-17 17:58:55 +0200

    Very ugly insee_filter

--- data_tmp/sources/insee_filtre/insee_filtre.py
+++ data_tmp/sources/insee_filtre/insee_filtre.py
@@ -1,40 +1,8 @@
-import os
-import json
 import pandas as pd
-from murs_invisibles import Processer
+from glob import glob
 
-file_dir = os.path.dirname(os.path.realpath(__file__))
-
-proc = Processer(
-    read_path=file_dir,
-    filter_indicator_path=None,
-    header=0,
-    encoding='utf-8',
-    sep='\t',
-    rename={
-        'country': 'country',
-        'year': 'year',
-        'indicator': 'indicator',
-        'value': 'value'
-    },
-    file_preprocess={
-        'insee_educ/cond-educ-dip-eleve-age-sexe-2.tsv': 'remove_prop',
-    },
-    file_postprocess={
-        'insee_educ/cond-educ-dip-eleve-age-sexe-2.tsv': 'perc',
-    },
-    file_valuemap={
-        'insee_educ/cond-educ-dip-eleve-age-sexe-2.tsv': 'proportion100',
-    },
-    file_save={
-        'insee_educ/cond-educ-dip-eleve-age-sexe-2.tsv': 'one_save',
-    },
-    file_min_year={
-        'insee_educ/cond-educ-dip-eleve-age-sexe-2.tsv': 2014,
-    },
-    country_filter_lang='fr',
-    country_lang='fr2fr',
-    indicator_lang='fr2fr',
-)
-
-proc.process()
+for p in glob("/Users/jimenarl/Desktop/chateau_ephemere_r2/murs_invisibles/data_tmp/m4l/insee_filtre/*/*.tsv"):
+    df = pd.read_csv(p, sep='\t')
+    df = df[df.year>=2014]
+    print(df)
+    df.to_csv(p, sep='\t', index=0, header=False)
