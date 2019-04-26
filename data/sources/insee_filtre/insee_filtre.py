421575a76956dd9f32d54ba682f031f2d6829353
jimenarl <jimena@melix.net>
2019-04-26 19:27:11 +0200

    Rmv unused

--- data/sources/insee_filtre/insee_filtre.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from glob import glob
-
-for p in glob("/Users/jimenarl/Desktop/chateau_ephemere_r2/murs_invisibles/data_tmp/m4l/insee_filtre/*/*.tsv"):
-    df = pd.read_csv(p, sep='\t')
-    df = df[df.year>=2014]
-    print(df)
-    df.to_csv(p, sep='\t', index=0, header=False)
