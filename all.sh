ef56c3cc2ec0b33f24dcd0dc2b4b39a522622323
jimenarl <jimena@melix.net>
2019-05-12 16:40:24 +0200

    Update paths

--- all.sh
+++ all.sh
@@ -1,29 +1,29 @@
 echo "> onu"
 
 echo ">> genderstats"
-PYTHONIOENCODING=UTF-8 python data_tmp/sources/onu/genderstats/genderstats.py
+PYTHONIOENCODING=UTF-8 python data/sources/onu/genderstats/genderstats.py
 echo ">> dataunorg"
-PYTHONIOENCODING=UTF-8 python data_tmp/sources/onu/dataunorg/dataunorg.py
+PYTHONIOENCODING=UTF-8 python data/sources/onu/dataunorg/dataunorg.py
 
 
 echo "> oecd"
 
 echo ">> developpement"
-PYTHONIOENCODING=UTF-8 python data_tmp/sources/oecd/developpement/developpement.py
+PYTHONIOENCODING=UTF-8 python data/sources/oecd/developpement/developpement.py
 
 echo ">> gouvernance"
-PYTHONIOENCODING=UTF-8 python data_tmp/sources/oecd/gouvernance/gouvernance.py
+PYTHONIOENCODING=UTF-8 python data/sources/oecd/gouvernance/gouvernance.py
 
 echo ">> emploi"
-PYTHONIOENCODING=UTF-8 python data_tmp/sources/oecd/emploi/emploi.py
+PYTHONIOENCODING=UTF-8 python data/sources/oecd/emploi/emploi.py
 
 echo ">> education"
-PYTHONIOENCODING=UTF-8 python data_tmp/sources/oecd/education/education.py
+PYTHONIOENCODING=UTF-8 python data/sources/oecd/education/education.py
 
 echo ">> entrepreneuriat"
-PYTHONIOENCODING=UTF-8 python data_tmp/sources/oecd/entrepreneuriat/entrepreneuriat.py
+PYTHONIOENCODING=UTF-8 python data/sources/oecd/entrepreneuriat/entrepreneuriat.py
 
 
 echo "> secretariat_egalite_femmes_hommes"
 
-PYTHONIOENCODING=UTF-8 python data_tmp/sources/secretariat_egalite_femmes_hommes/minist_dossier.py
+PYTHONIOENCODING=UTF-8 python data/sources/secretariat_egalite_femmes_hommes/minist_dossier.py
