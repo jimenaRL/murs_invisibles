51eda4e0f7ebc9b9b0259e7ca493738d020a5de6
jimenarl <jimena@melix.net>
2019-05-12 18:39:38 +0200

    Robustify shell scripts

--- all.sh
+++ all.sh
@@ -1,29 +1,32 @@
+#!/bin/bash
+BASEPATH=$(dirname $0)
+
 echo "> onu"
 
 echo ">> genderstats"
-PYTHONIOENCODING=UTF-8 python data/sources/onu/genderstats/genderstats.py
+PYTHONIOENCODING=UTF-8 python $BASEPATH/data/sources/onu/genderstats/genderstats.py
 echo ">> dataunorg"
-PYTHONIOENCODING=UTF-8 python data/sources/onu/dataunorg/dataunorg.py
+PYTHONIOENCODING=UTF-8 python $BASEPATH/data/sources/onu/dataunorg/dataunorg.py
 
 
 echo "> oecd"
 
 echo ">> developpement"
-PYTHONIOENCODING=UTF-8 python data/sources/oecd/developpement/developpement.py
+PYTHONIOENCODING=UTF-8 python $BASEPATH/data/sources/oecd/developpement/developpement.py
 
 echo ">> gouvernance"
-PYTHONIOENCODING=UTF-8 python data/sources/oecd/gouvernance/gouvernance.py
+PYTHONIOENCODING=UTF-8 python $BASEPATH/data/sources/oecd/gouvernance/gouvernance.py
 
 echo ">> emploi"
-PYTHONIOENCODING=UTF-8 python data/sources/oecd/emploi/emploi.py
+PYTHONIOENCODING=UTF-8 python $BASEPATH/data/sources/oecd/emploi/emploi.py
 
 echo ">> education"
-PYTHONIOENCODING=UTF-8 python data/sources/oecd/education/education.py
+PYTHONIOENCODING=UTF-8 python $BASEPATH/data/sources/oecd/education/education.py
 
 echo ">> entrepreneuriat"
-PYTHONIOENCODING=UTF-8 python data/sources/oecd/entrepreneuriat/entrepreneuriat.py
+PYTHONIOENCODING=UTF-8 python $BASEPATH/data/sources/oecd/entrepreneuriat/entrepreneuriat.py
 
 
 echo "> secretariat_egalite_femmes_hommes"
 
-PYTHONIOENCODING=UTF-8 python data/sources/secretariat_egalite_femmes_hommes/minist_dossier.py
+PYTHONIOENCODING=UTF-8 python $BASEPATH/data/sources/secretariat_egalite_femmes_hommes/minist_dossier.py
--- set_env.sh
+++ set_env.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+BASEPATH=$(dirname $0)
+
 export PYTHONIOENCODING=UTF-8
-export PYTHONPATH=$PYTHONPATH:/Users/jimenarl/Desktop/git_murs
+export PYTHONPATH=$PYTHONPATH:$BASEPATH/sources/python
 conda activate py3
