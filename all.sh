4a5dc0663f576d4c72ba397f061b8e5527e5cfaa
jimenarl <jimena@melix.net>
2019-06-21 02:28:45 +0200

    Ahhhhhhhh

--- all.sh
+++ all.sh
@@ -32,6 +32,18 @@ PYTHONIOENCODING=UTF-8 python $BASEPATH/data/sources/CNC-Audiens/audiens/cnc-aud
 PYTHONIOENCODING=UTF-8 python $BASEPATH/data/sources/CNC-Audiens/cnc1/cnc-audiens.py
 PYTHONIOENCODING=UTF-8 python $BASEPATH/data/sources/CNC-Audiens/cnc2/cnc-audiens.py
 
+echo "> EESR"
+
+echo ">> EESR-FR"
+PYTHONIOENCODING=UTF-8 python data/sources/EESR/EESR-FR/data.py
+
+echo ">> parite2019"
+PYTHONIOENCODING=UTF-8 python data/sources/EESR/EESR-FR/data.py
+
+echo "> secretariat_egalite_femmes_hommes_2"
+
+PYTHONIOENCODING=UTF-8 python $BASEPATH/data/sources/secretariat_egalite_femmes_hommes_2/secret.py
+
 
 echo "> secretariat_egalite_femmes_hommes"
 
