79e92596940a40ca4d6dae7512f23ed99195f6e5
jimenarl <wjimena@gmail.com>
2019-08-21 13:41:00 -0400

    config GM

--- configs/gm/gm.sh
+++ configs/gm/gm.sh
@@ -1,3 +1,4 @@
-# GM 
-python server.py -in_line_breaks 8 -out_line_breaks 8 -max_char 8 -port 9101
-python server.py -in_line_breaks 8 -out_line_breaks 8 -max_char 8 -port 9102
+# case gauche
+python server.py -in_line_breaks 10 -out_line_breaks 12 -max_char 6 -port 9101
+# case droite
+python server.py -in_line_breaks 10 -out_line_breaks 12 -max_char 6 -port 9102
