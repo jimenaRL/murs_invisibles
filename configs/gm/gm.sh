f65ae90c9757e665c54ed195ea74f8a88189889f
jimenarl <wjimena@gmail.com>
2019-08-21 11:32:45 -0400

    Add congifs

---
+++ configs/gm/gm.sh
@@ -0,0 +1,3 @@
+# GM 
+python server.py -in_line_breaks 8 -out_line_breaks 8 -max_char 8 -port 9101
+python server.py -in_line_breaks 8 -out_line_breaks 8 -max_char 8 -port 9102
---
+++ configs/jardin-face/jardin-face.sh
@@ -0,0 +1,2 @@
+# TJR - face jardin 
+python server.py -in_line_breaks 4 -out_line_breaks 4 -max_char 20  -indent 0 -port 910
