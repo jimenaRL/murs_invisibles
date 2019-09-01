a61b83526a09ff7bd7307dd2851f45fff91dad8c
jimenarl <jimena@melix.net>
2019-09-01 17:47:21 +0200

    Imperceptible changes

--- sources/python/murs_invisibles/processing/filter.py
+++ sources/python/murs_invisibles/processing/filter.py
@@ -16,14 +16,14 @@ class Filter(object):
                 'table_min_year': {
                     'filename': 2010
                 }
-                'country_filter_lang': 'fr',
+                'origin_language': 'fr',
                 'filter_indicator_path': None,
             }
         """
 
         self.years = config['year']
         self.filter_country_path = FILTER_COUNTRY_PATH.format(
-            config['country_filter_lang'])
+            config['origin_language'])
 
         if config['filter_indicator_path']:
             with open(config['filter_indicator_path'], 'r',
--- sources/python/murs_invisibles/processing/processer.py
+++ sources/python/murs_invisibles/processing/processer.py
@@ -1,4 +1,6 @@
 import os
+from copy import deepcopy
+import pandas as pd
 
 from murs_invisibles.processing.mapper import Mapper
 from murs_invisibles.processing.inout import IO
@@ -9,13 +11,17 @@ from murs_invisibles.processing.translator import Translator
 from murs_invisibles.processing.sorter import Sorter
 
 
+COUNTRY_PATH = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'auxiliar/translators/country_{}.json')
+
 class Processer(object):
 
     def __init__(self, config):
         """
         Config example:
             {
-                "data_path": "path/to/folder/with/raw/data",
+                "base_path": "path/to/folder/with/raw/data",
                 "io": {
                     "header": 1,
                     "encoding": 'latin1',
@@ -58,15 +64,53 @@ class Processer(object):
             }
         """
 
-        self.data_path = config['data_path']
+        self.config = config
+        self.base_path = config['base_path']
         self.tables = config['io']['fns'].keys()
 
         self.io = IO(config['io'])
         self.preprocesser = PreProcesser(config['preprocesser'])
+
+        if not "origin_language" in config['filter']:
+            config['filter'].update({
+                "origin_language": config["origin_language"]
+                })
         self.filter = Filter(config['filter'])
+
         self.mapper = Mapper(config['mapper'])
+
+        default_translation = '{}2{}'.format(
+                config["origin_language"],
+                config["target_language"])
+
+        IND_PATH = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            'auxiliar/translators/indicator_{}.json')
+
+        if not "indicator" in config['translator']:
+            config['translator'].update({
+                "indicator": default_translation
+            })
+        config['translator'].update({
+            # "ind_dict_path": IND_PATH.format(default_translation)
+
+            "ind_dict_path": os.path.join(
+                self.base_path,
+                "indicator_translations.csv")
+            })
+
+        if not "country" in config['translator']:
+            config['translator'].update({
+                "country": default_translation
+                })
+        config['translator'].update({
+                "country_dict_path": COUNTRY_PATH.format(default_translation)
+                })
+
         self.translator = Translator(config['translator'])
+
         self.postprocesser = PostProcesser(config['postprocesser'])
+
         self.sorter = Sorter(config['sorter'])
 
     def process(self):
@@ -74,7 +118,7 @@ class Processer(object):
         for table in self.tables:
 
             print(f"***** {table} ****")
-            path = os.path.join(self.data_path, table)
+            path = os.path.join(self.base_path, table)
 
             # load
             df = self.io.load(path)
