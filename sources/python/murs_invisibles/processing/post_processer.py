dd58363c6d2baf5dd22e806bc4c06a54439e616e
jimenarl <jimena@melix.net>
2019-10-27 14:35:51 +0100

    Update virg after

--- sources/python/murs_invisibles/processing/post_processer.py
+++ sources/python/murs_invisibles/processing/post_processer.py
@@ -27,7 +27,7 @@ class PostProcesser(object):
         df['sign'] = df.apply(
             lambda row: '+' if row.value >= 0 else '-', axis=1)
         df['value'] = df.apply(
-            lambda row: row.sign + '%1.2f' % abs(row.value) + '%', axis=1)
+            lambda row: row.sign + '%1.1f' % abs(row.value) + '%', axis=1)
         return df
 
     @classmethod
@@ -48,7 +48,7 @@ class PostProcesser(object):
         df['value'] = df.apply(
             lambda row: row.value / (1 + row.value), axis=1)
         df['value'] = df.apply(
-            lambda row: '%1.2f' % abs(100 * row.value) + '%', axis=1)
+            lambda row: '%1.0f' % abs(100 * row.value) + '%', axis=1)
         return df
 
     @classmethod
