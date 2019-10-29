156069eff86da702cb545f844ccc830cbf8039f0
jimenarl <jimena@melix.net>
2019-10-29 18:44:23 +0100

    saaires hoaraires

--- data/sources/INSEE2/insee2.py
+++ data/sources/INSEE2/insee2.py
@@ -18,6 +18,7 @@ config = {
             "Revenus_Salaires.csv": "one_save",
             "Travail_Emploi.csv": "one_save",
             "Conditions de vie_2015 - temps_sociaux.csv": "one_save",
+            "marc-salair-net-mens-sexe-csp-2 - hor.csv": "one_save",
         },
     },
     "preprocesser": {
@@ -27,6 +28,7 @@ config = {
             "Revenus_Salaires.csv": ["virg2point"],
             "Travail_Emploi.csv": ["virg2point"],
             "Conditions de vie_2015 - temps_sociaux.csv": [],
+            "marc-salair-net-mens-sexe-csp-2 - hor.csv": ["virg2point", "remove_euro_and_perc", "percRel100"],
         },
         'rename': {
             'country': 'pays',
@@ -42,7 +44,8 @@ config = {
             "Enseignement_Education.csv": 2010,
             "Revenus_Salaires.csv": 2010,
             "Travail_Emploi.csv": 2010,
-            "Conditions de vie_2015 - temps_sociaux.csv": 1980
+            "Conditions de vie_2015 - temps_sociaux.csv": 1980,
+            "marc-salair-net-mens-sexe-csp-2 - hor.csv": 1980,
         }
     },
     "mapper": {
@@ -52,6 +55,7 @@ config = {
             "Revenus_Salaires.csv": "diffFH_20",
             "Travail_Emploi.csv": "diffFH_20",
             "Conditions de vie_2015 - temps_sociaux.csv": "diff_fm_minutes",
+            "marc-salair-net-mens-sexe-csp-2 - hor.csv": "diffFH_20",
         }
     },
     "translator": {
@@ -63,6 +67,7 @@ config = {
             "Revenus_Salaires.csv": "diff_perc",
             "Travail_Emploi.csv": "diff_pp",
             "Conditions de vie_2015 - temps_sociaux.csv": "diff_minutes",
+            "marc-salair-net-mens-sexe-csp-2 - hor.csv": "diff_perc_0v",
         }
     },
     "sorter": {
@@ -72,6 +77,7 @@ config = {
             "Revenus_Salaires.csv": "none",
             "Travail_Emploi.csv": "none",
             "Conditions de vie_2015 - temps_sociaux.csv": "date_country",
+            "marc-salair-net-mens-sexe-csp-2 - hor.csv": "date_country",
         }
     },
 }
