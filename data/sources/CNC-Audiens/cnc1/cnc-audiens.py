be99f0d6ecf50e2924e968c81c80d52533ee654a
jimenarl <jimena@melix.net>
2019-10-30 13:04:30 +0100

    join cnc1 data

--- data/sources/CNC-Audiens/cnc1/cnc-audiens.py
+++ data/sources/CNC-Audiens/cnc1/cnc-audiens.py
@@ -12,25 +12,23 @@ config = {
         "header": 0,
         "encoding": 'utf-8',
         "fns": {
-            "Effectifs par catégorie professionnelle.csv": "one_save",
+            "Effectifs_par_catégorie_professionnelle_ET_Répartition_des_effectifs_selon_l’année_d’agrément_et_le_sexe.csv": "one_save",
             "Films d’initiative française selon le genre des films et le sexe des réalisateur-trices.csv": "one_save",
             # "Part des entrées des films réalisés par des femmes.csv": "one_save",
             "Part des films réalisés par des femmes - Europe.csv": "one_save",
             # "Part des films réalisés par des femmes .csv": "one_save",
             # "Réalisateur·trices de courts métrages.csv": "one_save",
-            "Répartition des effectifs selon l’année d’agrément et le sexe.csv": "one_save",
             # "écarts des salaires horaires médians des femmes et ceux des hommes dans le cinéma ciné.csv": "one_save",
         },
     },
     "preprocesser": {
         'fns': {
-            "Effectifs par catégorie professionnelle.csv": ["no_process"],
+            "Effectifs_par_catégorie_professionnelle_ET_Répartition_des_effectifs_selon_l’année_d’agrément_et_le_sexe.csv": ["virg2point"],
             "Films d’initiative française selon le genre des films et le sexe des réalisateur-trices.csv": ["no_process"],
             # "Part des entrées des films réalisés par des femmes.csv": "diffFH",
             "Part des films réalisés par des femmes - Europe.csv": ["no_process"],
             # "Part des films réalisés par des femmes .csv": ["no_process"],
             # "Réalisateur·trices de courts métrages.csv": ["no_process"],
-            "Répartition des effectifs selon l’année d’agrément et le sexe.csv": ["virg2point"],
             "écarts des salaires horaires médians des femmes et ceux des hommes dans le cinéma ciné.csv": ["virg2point"],
         },
         'rename': {
@@ -42,26 +40,24 @@ config = {
     },
     "mapper": {
         'fns': {
-            "Effectifs par catégorie professionnelle.csv": "proportion100",
+            "Effectifs_par_catégorie_professionnelle_ET_Répartition_des_effectifs_selon_l’année_d’agrément_et_le_sexe.csv": "proportion100",
             "Films d’initiative française selon le genre des films et le sexe des réalisateur-trices.csv": "proportion100",
             # "Part des entrées des films réalisés par des femmes.csv": "",
             "Part des films réalisés par des femmes - Europe.csv": "proportion100",
             # "Part des films réalisés par des femmes .csv": "proportion100",
             # "Réalisateur·trices de courts métrages.csv": "",
-            "Répartition des effectifs selon l’année d’agrément et le sexe.csv": "proportion100",
             # "écarts des salaires horaires médians des femmes et ceux des hommes dans le cinéma ciné.csv": "",
         }
     },
     "filter": {
         'filter_indicator_path': None,
         'year': {
-            "Effectifs par catégorie professionnelle.csv": 2010,
+            "Effectifs_par_catégorie_professionnelle_ET_Répartition_des_effectifs_selon_l’année_d’agrément_et_le_sexe.csv": 2010,
             "Films d’initiative française selon le genre des films et le sexe des réalisateur-trices.csv": 2010,
             # "Part des entrées des films réalisés par des femmes.csv": 2010,
             "Part des films réalisés par des femmes - Europe.csv": 2010,
             # "Part des films réalisés par des femmes .csv": 2010,
             # "Réalisateur·trices de courts métrages.csv": 2010,
-            "Répartition des effectifs selon l’année d’agrément et le sexe.csv": 2010,
             # "écarts des salaires horaires médians des femmes et ceux des hommes dans le cinéma ciné.csv": 2010,
         }
     },
@@ -69,25 +65,23 @@ config = {
     },
     "postprocesser": {
         'fns': {
-            "Effectifs par catégorie professionnelle.csv": "perc",
+            "Effectifs_par_catégorie_professionnelle_ET_Répartition_des_effectifs_selon_l’année_d’agrément_et_le_sexe.csv": "perc",
             "Films d’initiative française selon le genre des films et le sexe des réalisateur-trices.csv": "perc",
             # "Part des entrées des films réalisés par des femmes.csv": "",
             "Part des films réalisés par des femmes - Europe.csv": "perc",
             # "Part des films réalisés par des femmes .csv": "perc",
             # "Réalisateur·trices de courts métrages.csv": "",
-            "Répartition des effectifs selon l’année d’agrément et le sexe.csv": "perc",
             # "écarts des salaires horaires médians des femmes et ceux des hommes dans le cinéma ciné.csv": "",
         }
     },
     "sorter": {
         'fns': {
-            "Effectifs par catégorie professionnelle.csv": "none",
+            "Effectifs_par_catégorie_professionnelle_ET_Répartition_des_effectifs_selon_l’année_d’agrément_et_le_sexe.csv": "none",
             "Films d’initiative française selon le genre des films et le sexe des réalisateur-trices.csv": "none",
             # "Part des entrées des films réalisés par des femmes.csv": "",
             "Part des films réalisés par des femmes - Europe.csv": "none",
             # "Part des films réalisés par des femmes .csv": "none",
             # "Réalisateur·trices de courts métrages.csv": "",
-            "Répartition des effectifs selon l’année d’agrément et le sexe.csv": "none",
             # "écarts des salaires horaires médians des femmes et ceux des hommes dans le cinéma ciné.csv": "",
         }
     },
