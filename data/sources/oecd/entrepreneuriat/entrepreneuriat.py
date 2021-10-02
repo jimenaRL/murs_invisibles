e46adb3fe0a1677641871494956ec0d771f202f4
jimenaRL <jimena@melix.net>
2021-10-02 13:05:40 +0200

    order merge

--- data/sources/oecd/entrepreneuriat/entrepreneuriat.py
+++ data/sources/oecd/entrepreneuriat/entrepreneuriat.py
@@ -1,6 +1,4 @@
 import os
-import json
-import pandas as pd
 from murs_invisibles import Processer
 
 file_dir = os.path.dirname(os.path.realpath(__file__))
@@ -9,18 +7,22 @@ filter_indicator_path = os.path.join(file_dir, 'indicator_filter.txt')
 config = {
     "base_path": file_dir,
     "origin_language": "fr",
-    "merge": {
-        'ENT4_GIDDB2019_poste_de_direction': [
-            'ENT4.csv',
-            'GIDDB2019_poste_de_direction.csv',
-        ]
-    },
+    "merge": [
+        {
+            'name': 'ENT4_GIDDB2019_poste_de_direction',
+            'datas': [
+                'ENT4.csv',
+                'GIDDB2019_poste_de_direction.csv',
+            ],
+            'sorter': "date_country"
+        },
+    ],
     "io": {
         "header": 0,
         "encoding": 'utf-8',
         "fns": {
             'ENT4.csv': 'one_save',
-           # 'GENDER_ENT1_01042019230825609_remove_ENT4.csv': 'sep_save',
+            'GENDER_ENT1_01042019230825609_remove_ENT4.csv': 'sep_save',
             'GIDDB2019_poste_de_direction.csv': 'one_save',
         },
     },
@@ -69,6 +71,7 @@ config = {
             'ENT4.csv': 'date_country',
             'GENDER_ENT1_01042019230825609_remove_ENT4.csv': 'date_country',
             'GIDDB2019_poste_de_direction.csv': 'date_country',
+            'ENT4_GIDDB2019_poste_de_direction': 'country',
         }
     },
 }
