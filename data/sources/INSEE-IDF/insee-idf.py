134d29adf9a03b2ecf720f1726053953e390cb68
jimenaRL <jimena@melix.net>
2021-10-02 14:54:46 +0200

    DATA insse-idf

--- data/sources/INSEE-IDF/insee-idf.py
+++ data/sources/INSEE-IDF/insee-idf.py
@@ -7,16 +7,28 @@ filter_indicator_path = os.path.join(file_dir, 'indicator_filter.txt')
 config = {
     "base_path": os.path.dirname(os.path.realpath(__file__)),
     "origin_language": "fr",
+    "merge": [
+        {
+            'name': 'insee_idf',
+            'datas': [
+                "INSEE-IDF - conseils municipaux_2021.csv",
+                "INSEE-IDF - Part Femmes Cube & CSP Hauts de seine.csv",
+            ],
+            'sorter': "date_country"
+        },
+    ],
     "io": {
         "header": 0,
         "encoding": 'utf-8',
         'fns': {
             "INSEE-IDF - conseils municipaux_2021.csv": "one_save",
+            "INSEE-IDF - Part Femmes Cube & CSP Hauts de seine.csv": "one_save",
         },
     },
     "preprocesser": {
         'fns': {
             "INSEE-IDF - conseils municipaux_2021.csv": ["virg2point"],
+            "INSEE-IDF - Part Femmes Cube & CSP Hauts de seine.csv": ["remove_prop"],
         },
         'rename': {
             'country': ['pays'],
@@ -29,11 +41,13 @@ config = {
         'filter_indicator_path': filter_indicator_path,
         'year': {
             "INSEE-IDF - conseils municipaux_2021.csv": 2010,
+            "INSEE-IDF - Part Femmes Cube & CSP Hauts de seine.csv": 2010,
         }
     },
     "mapper": {
         'fns': {
             "INSEE-IDF - conseils municipaux_2021.csv": "proportion100",
+            "INSEE-IDF - Part Femmes Cube & CSP Hauts de seine.csv": "proportion100",
         }
     },
     "translator": {
@@ -41,12 +55,14 @@ config = {
     "postprocesser": {
         'fns': {
             "INSEE-IDF - conseils municipaux_2021.csv": "perc",
-
+            "INSEE-IDF - Part Femmes Cube & CSP Hauts de seine.csv": "perc",
         }
     },
     "sorter": {
         'fns': {
             "INSEE-IDF - conseils municipaux_2021.csv": "none",
+            "INSEE-IDF - Part Femmes Cube & CSP Hauts de seine.csv": "none",
+            "insee_idf": "none"
         }
     },
 }
